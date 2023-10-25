68d86431562e8cf99a97afe988597adf8006326f
EtienneChollet <etiennechollet2626@gmail.com>
2023-10-24 22:18:34 -0400

    Removed some extraneous json files and renamed scripts to be ordered numerically

--- scripts/3_training/train.sh
+++ scripts/3_training/train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-jobsubmit -A psoct -p rtx8000 -m 20G -t 1-00:00:00 -c 1 -G 1 -o logs/train.log python3 scripts/training/train.py;
+jobsubmit -A psoct -p rtx8000 -m 20G -t 1-00:00:00 -c 1 -G 1 -o logs/train.log python3 scripts/3_training/train.py;
 watch -n 0.1 "squeue -u $USER"
--- scripts/4_testing/test.sh
+++ scripts/4_testing/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-jobsubmit -A psoct -p rtx8000 -m 10G -t 1-00:00:00 -c 1 -G 1 -o logs/test.log python3 scripts/testing/test.py;
+jobsubmit -A psoct -p rtx8000 -m 10G -t 1-00:00:00 -c 1 -G 1 -o logs/test.log python3 scripts/4_testing/test.py;
 watch -n 0.1 "squeue -u $USER"
